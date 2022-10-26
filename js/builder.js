2581923fbd1b59e7d25097096b19e505f4f7b5f9
noverlean <115197557+noverlean@users.noreply.github.com>
2022-10-26 17:05:17 +0300

    Add files via upload

---
+++ js/builder.js
@@ -0,0 +1,17 @@
+window.onload = buildProducts(); //products.length
+
+function buildProducts(){
+    for (var i = 0; i < menu.length; i++){
+        document.querySelector('.products').insertAdjacentHTML(
+            'afterbegin',
+            '<div class="currentProduct">' +
+            '    <div class="title">' +
+            '        <div class="name">' + menu[i].title + '</div>' +
+            '        <div class="price">' + menu[i].price + '$</div>' +
+            '    </div>' +
+                menu[i].content +
+            '    <a class="buyBtn" href="sendForm.html">Заказать</a>' +
+            '</div>'
+        );
+    }          
+}
